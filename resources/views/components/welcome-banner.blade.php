ab3e8823941944ec703f056f36d4b916a141b2eb
Infinite0011 <dmytro.savuliak1998@gmail.com>
2023-09-14 14:51:55 -0400

    add banner

--- resources/views/components/welcome-banner.blade.php
+++ resources/views/components/welcome-banner.blade.php
@@ -1,40 +1,39 @@
 <section class="bg-gray-50">
-    <div class="max-w-screen-xl px-4 py-32 mx-auto sm:px-6 lg:px-8">
-        <div class="max-w-xl mx-auto text-center">
-            <h1 class="text-3xl font-extrabold sm:text-5xl">
-                Welcome to the
+    <div class="bg-[#fafcff]">
+		<div class="bg-center bg-cover bg-no-repeat flex flex-col justify-center items-center m-auto min-h-[700px] bg-[url('/assets/frontend/images/sugarmd-dr-ergin-cover-photo.jpg')]">
+            <div class="max-w-screen-2xl w-full px-4 py-32 mx-auto sm:px-6 lg:px-8">
+                <h1 class="text-start text-6xl font-semibold mb-4 pt-14 text-[#1f387e]">
+                    <span class="text-5xl font-normal">Meet Our Medicinal Herbal</span><br>Supplements &amp; Vitamins        </h1>
+                <h2 class="text-xl font-black mb-7 text-white text-left" style="text-shadow:0.05em 0 black, 0 0.05em black, -0.05em 0 black, 0 -0.05em black, -0.05em -0.05em black, -0.05em 0.05em black, 0.05em -0.05em black, 0.05em 0.05em black;">
+                    Let food be thy medicine and medicine be thy food.            <br> <strong>- Hippocrates</strong></h2>
+                <a href="https://sugarmds.com/shop" class="max-w-[182px] flex text-center items-center justify-center leading-8 uppercase w-full m-0 font-bold text-xs p-2.5 rounded-xl bg-[#146ffe] text-white">Shop Our Products</a>
+            </div>
+        </div>
+        <div class="py-5 bg-[#ebf0f8]" style="margin-bottom:20px;">
+            <div class="max-w-screen-2xl w-full px-4 mx-auto sm:px-6 lg:px-8">
+                <div class="row flex align-items">
+                    <div class="w-1/2">
+                        <h3 class="text-2xl font-semibold text-[#1f387e] text-left pb-2.5">Subscribe to SugarMD newsletter</h3>
+                        <p class="text-sm text-[#32457a] max-w-[470px] text-left">Receive SugarMD newsletter, savings, exclusive offers and more!</p>
+                    </div>
+                    <div class="w-1/2">
+                        <form>
+                            <div>
+                                <div>
+                                </div>
+                                <div class="max-w-[500px] mx-auto">
+                                    <input type="email" class="px-5 py-4 relative w-[65%] max-w-full left-1 border-[1.5px] rounded-l text-xs h-12 border-[#0bb1fc]" name="email" placeholder="Please enter your email" required="" aria-label="please enter your email" data-uw-rm-form="fx">
+                                    <a href="https://manage.kmail-lists.com/subscriptions/subscribe?a=UTkMAa&amp;g=VkS5uy" class="bg-white text-[#1f387e] -mt-3 rounded-r text-xs transition px-5 py-4 border-[1.5px] border-[#0bb1fc]" data-uw-rm-form="submit">Subscribe</a>
+                                </div>
+                                <div>
+                                </div>
+                            </div>
 
-                <span class="text-indigo-600">
-                    Lunar
-                </span>
+                        </form>
+                    </div>
 
-                Demo Store
+                </div>
 
-                <span role="img"
-                      aria-hidden="true">
-                    👋
-                </span>
-            </h1>
-
-            <p class="mt-4 font-medium sm:leading-relaxed sm:text-xl">
-                This is an example of a classic e-commerce store built with Lunar.
-                We are currently making a screencast series to show you how we did it!
-            </p>
-
-            <div class="flex flex-wrap justify-center gap-4 mt-8">
-                <a class="block w-full px-12 py-3 font-medium text-white bg-indigo-600 rounded shadow sm:w-auto active:bg-indigo-500 hover:bg-indigo-700 focus:outline-none focus:ring"
-                   href="https://lunarphp.io/"
-                   target="_blank"
-                   rel="noopener noreferrer">
-                    Lunar Website
-                </a>
-
-                <a class="block w-full px-12 py-3 font-medium text-indigo-600 rounded shadow sm:w-auto hover:text-indigo-700 active:text-indigo-500 focus:outline-none focus:ring"
-                   href="https://github.com/lunarphp/demo-store"
-                   target="_blank"
-                   rel="noopener noreferrer">
-                    Demo Store Repo
-                </a>
             </div>
         </div>
     </div>
